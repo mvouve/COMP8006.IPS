5bd199761f4bf95afb6ce7415d7ed17d5171c492
Marc Vouve <mvouve@gmail.com>
2016-02-28 18:46:57 -0800

    Completed Basic assignment

--- main.go
+++ main.go
@@ -1,76 +1,179 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"time"
 
 	"github.com/Unknwon/goconfig"
 )
 
 const configIni string = "config.ini"
+const defaultTrys int = 5
 
 var configure *goconfig.ConfigFile
 
 type manifestType struct {
-	filePos int64
-	bans    map[time.Time]string
+	FilePos int64
+	Bans    map[string]time.Time
+	Events  map[string][]time.Time
 }
 
-func man() {
+func main() {
 	var manifest manifestType
+	var err error
 
-	configure, err := goconfig.LoadConfigFile(configIni)
+	configure, err = goconfig.LoadConfigFile(configIni)
 	if os.IsNotExist(err) {
 		log.Fatal("config.ini should be in the same directory as this is run from")
 	}
 
-	filePath, _ := configure.GetValue("manifest", "file")
+	filePath := configure.MustValue("manifest", "file", "manifest")
 	if manifestFile, err := os.Open(filePath); os.IsNotExist(err) {
 		manifest = initManifest()
 	} else {
 		manifest = loadManifest(manifestFile)
 	}
-	checkBans(&manifest.bans)
-	checkSecure(&manifest.filePos)
-
+	checkBans(&manifest.Bans)
+	checkSecure(&manifest.FilePos, manifest.Events)
+	checkEvents(manifest.Bans, manifest.Events)
+	save(filePath, manifest)
 }
 
 func initManifest() manifestType {
-	return manifestType{}
+	events := make(map[string][]time.Time)
+	bans := make(map[string]time.Time)
+
+	return manifestType{Events: events, Bans: bans}
 }
 
 func loadManifest(file *os.File) manifestType {
-	return manifestType{}
+	stats, _ := file.Stat()
+	buffer := make([]byte, stats.Size())
+	file.Read(buffer)
+	mani := manifestType{}
+	err := json.Unmarshal(buffer, &mani)
+	if err != nil {
+		mani = initManifest()
+		log.Println(err)
+	}
+	return mani
 }
 
-func checkBans(currentBans *map[time.Time]string) {
-	for expiry, ip := range *currentBans {
+func checkBans(currentBans *map[string]time.Time) {
+	for ip, expiry := range *currentBans {
 		if time.Now().Before(expiry) {
 			dropBan(ip)
-			delete(*currentBans, expiry)
+			delete(*currentBans, ip)
+		}
+	}
+}
+
+func checkSecure(filePos *int64, events map[string][]time.Time) {
+	filepath := configure.MustValue("auth", "file", "/var/log/secure")
+	logFile, err := os.Open(filepath)
+	if os.IsNotExist(err) {
+		log.Fatalln("secure file not found, if rsyslog is installed check the ini")
+	} else if err != nil {
+		log.Fatalln(err)
+	}
+	defer logFile.Close()
+
+	if _, err = logFile.Seek(*filePos, os.SEEK_SET); err != nil {
+		logFile.Seek(0, 0)
+		*filePos = 0
+	}
+	logBuff := bufio.NewReader(logFile)
+	for {
+		line, err := logBuff.ReadString('\n')
+		*filePos += int64(len(line))
+		if err == io.EOF {
+			break
+		}
+		if isFailedLogin(line) {
+			if events[getIPfromString(line)] == nil {
+				events[getIPfromString(line)] = make([]time.Time, 0, 1024)
+			}
+			events[getIPfromString(line)] = append(events[getIPfromString(line)][0:], getTimeFromString(line))
+		}
+	}
+
+}
+
+func checkEvents(bans map[string]time.Time, events map[string][]time.Time) {
+	now, _ := time.Parse("Jan 02 15:04:05", time.Now().Format("Jan 02 15:04:05"))
+	for ip := range events {
+		for idx := 0; idx < len(events[ip]); idx++ {
+
+			if now.Sub(events[ip][idx]) > time.Hour { // more than a minute has passed.
+				events[ip] = append(events[ip][:idx], events[ip][idx+1:]...)
+				idx--
+			}
+		}
+		if len(events[ip]) >= configure.MustInt("auth", "max_attempts", defaultTrys) {
+			addBan(ip, bans)
 		}
 	}
 }
 
 func dropBan(ip string) {
 	for _, chain := range []string{"INPUT", "OUTPUT", "FORWARD"} {
-		exec.Command("iptables", "-D", chain, "-s", ip, "-j", "DROP")
+		exec.Command("iptables", "-D", chain, "-s", ip, "-j", "DROP").Run()
 	}
 }
 
-func addBan(ip string) {
-	for _, chain := range []string{"INPUT", "OUTPUT", "FORWARD"} {
-		exec.Command("iptables", "-A", chain, "-s", ip, "-j", "DROP")
+func addBan(ip string, bans map[string]time.Time) {
+	_, ok := bans[ip]
+	fmt.Println(bans)
+	fmt.Println(ok)
+	if !ok {
+		for _, chain := range []string{"INPUT", "OUTPUT", "FORWARD"} {
+			exec.Command("iptables", "-A", chain, "-s", ip, "-j", "DROP").Run()
+		}
 	}
+	bans[ip] = time.Now().Add(time.Hour * 24)
 }
 
-func checkSecure(filePos *int64) {
-	filepath, _ := configure.GetValue("auth", "file")
-	logFile, err := os.Open(filepath)
-	if os.IsNotExist(err) {
-		log.Fatalln("secure file not found, if rsyslog is installed check the ini")
+func isFailedLogin(log string) bool {
+	failed, _ := regexp.MatchString("Failed password", log)
+	return failed
+}
+
+func getIPfromString(log string) string {
+	regx := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}`)
+
+	return regx.FindString(log)
+}
+
+func getTimeFromString(log string) time.Time {
+	t, _ := time.Parse("Jan 02 15:04:05", getTimeStringFromString(log))
+	fmt.Println(t)
+
+	return t
+}
+
+func getTimeStringFromString(line string) string {
+	regx := regexp.MustCompile(`[A-Za-z]{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}`)
+
+	return regx.FindString(line)
+}
+
+func save(f string, m manifestType) {
+	data, err := json.Marshal(m)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	logFile.Seek(*filePos, 0)
+	file, _ := os.Create(f)
+
+	var out bytes.Buffer
+	json.Indent(&out, data, "", "\t")
+	out.WriteTo(file)
+	file.Close()
 }

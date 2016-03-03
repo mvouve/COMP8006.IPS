6a5971e0a22d8c034a7f314f3b76de8ca7a2c9ed
Marc Vouve <mvouve@gmail.com>
2016-03-02 21:58:52 -0800

    Deliverables Completed

--- html/script/ips.js
+++ html/script/ips.js
@@ -1,3 +1,23 @@
+/*------------------------------------------------------------------------------
+-- DATE:	       February 28, 2016
+--
+-- Source File:	 ips.js
+--
+-- REVISIONS:
+--
+-- DESIGNER:	   Marc Vouve
+--
+-- PROGRAMMER:	 Marc Vouve
+--
+--
+-- INTERFACE:
+--	hostList.currentBan = function(host)
+--  hostList.events = function(host)
+--  hostList.hosts = function()
+--
+--
+-- NOTES: This is the main angular logic file.
+------------------------------------------------------------------------------*/
 angular.module('ipsApp', [])
   .controller('HostListController', function($scope, $http, $interval) {
 
@@ -15,6 +35,24 @@ angular.module('ipsApp', [])
         });
     }, 1000);
 
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    currentBan
+--
+-- DATE:        February 25, 2016
+--
+-- REVISIONS:	  (none)
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:		hostList.currentBan = function(host)
+--      host:   the host to check against.
+--
+-- RETURNS: 		the time that the ban will end (if the host is banned)
+--
+-- NOTES:			  if no ban is found "nothing" is returned.
+------------------------------------------------------------------------------*/
     hostList.currentBan = function(host) {
       var t;
       angular.forEach(hostList.manifest.Bans, function(value, key) {
@@ -25,6 +63,24 @@ angular.module('ipsApp', [])
       return t
     };
 
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    currentBan
+--
+-- DATE:        February 25, 2016
+--
+-- REVISIONS:	  (none)
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:		hostList.events = function(host)
+--      host:   the host to check against.
+--
+-- RETURNS: 		recent events from the host.
+--
+-- NOTES:			  if no events is found "nothing" is returned.
+------------------------------------------------------------------------------*/
     hostList.events = function(host) {
       var e = []
 
--- manifest_helper.go
+++ manifest_helper.go
@@ -1,3 +1,23 @@
+/*------------------------------------------------------------------------------
+-- DATE:	       March 1, 2016
+--
+-- Source File:	 manifest_helper.go
+--
+-- REVISIONS:
+--
+-- DESIGNER:	   Marc Vouve
+--
+-- PROGRAMMER:	 Marc Vouve
+--
+--
+-- INTERFACE:
+--	func initManifest() manifestType
+--  func loadManifest(file *os.File) manifestType
+--  func (m manifestType) save(f string)
+--
+--
+-- NOTES: This file is the main file in the IPS suite.
+------------------------------------------------------------------------------*/
 package main
 
 import (
@@ -8,6 +28,23 @@ import (
 	"time"
 )
 
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    initManifest
+--
+-- DATE:        February 25, 2016
+--
+-- REVISIONS:	  (none)
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:		func initManifest() manifestType
+--
+-- RETURNS: 		manifestType a new manifest.
+--
+-- NOTES:			This function creates a new manifest.
+------------------------------------------------------------------------------*/
 func initManifest() manifestType {
 	events := make(map[string][]time.Time)
 	bans := make(map[string]time.Time)
@@ -15,6 +52,24 @@ func initManifest() manifestType {
 	return manifestType{Events: events, Bans: bans}
 }
 
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    loadManifest
+--
+-- DATE:        February 25, 2016
+--
+-- REVISIONS:	  (none)
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:		func loadManifest(file *os.File) manifestType
+--		  file:		a file to load the manifest from.
+--
+-- RETURNS: 		manifestType - the manifest loaded from the file
+--
+-- NOTES:			This function loads a manfiest from a file.
+------------------------------------------------------------------------------*/
 func loadManifest(file *os.File) manifestType {
 	stats, _ := file.Stat()
 	buffer := make([]byte, stats.Size())
@@ -28,6 +83,24 @@ func loadManifest(file *os.File) manifestType {
 	return mani
 }
 
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    save
+--
+-- DATE:        February 25, 2016
+--
+-- REVISIONS:	  (none)
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:		func (m manifestType) save(f string)
+--				 f:   name of the file to write to, doesn't need to exist.
+--
+-- RETURNS: 		void
+--
+-- NOTES:			saves the manifest to a file
+------------------------------------------------------------------------------*/
 func (m manifestType) save(f string) {
 	data, err := json.Marshal(m)
 	if err != nil {
